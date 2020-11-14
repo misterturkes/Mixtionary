6f40d173ddb8b32961d989b3305c4c4b39073aee
Diego-cs-java <73859804+Diego-cs-java@users.noreply.github.com>
2020-11-13 17:45:09 -0800

    Add files via upload

--- team-03-04/src/application/MixionaryController.java
+++ team-03-04/src/application/MixionaryController.java
@@ -1,5 +1,43 @@
 package application;
 
-public class MixionaryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MixionaryController implements Initializable{
+
+	
+	// Enables the "My Recipes" button in the Mixionary.fxml to change scenes to myRecipe.fxml
+	public void changeSceneWhenPushed(ActionEvent event) throws IOException{
+		
+		Parent myRecipeView = FXMLLoader.load(getClass().getResource("myRecipe.fxml"));
+		Scene myRecipeViewScene = new Scene (myRecipeView);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(myRecipeViewScene);
+		window.show();
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 	
 }
